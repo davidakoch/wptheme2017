d6eb2cceb1a61488aaa90e2381d6c4f9acc871f6
David A Koch <kodave1@gmail.com>
2017-10-13 23:14:45 -0400

    added wp head

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<meta name="description" content="wordpress theme">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"
+	<?php wp_head(); ?>
 </head>
 	<body>
 		<header>
