7ef775f8f7b2e5a24fdc6806b54d3ce8645eb29b
David A Koch <kodave1@gmail.com>
2017-10-13 22:29:01 -0400

    added to index php

--- index.php
+++ index.php
@@ -1,1 +1,119 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Custom-made WordPress Template</title>
+	<meta name="description" content="">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
+</head>
+	<body>
+		<header>
+			<div>
+			<nav id="top_header">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-8 main_nav">
+							<ul>
+								<li><a>Home</a></li>
+								<li><a>About Us</a></li>
+								<li><a>Blog</a></li>
+								<li><a>Contact Us</a></li>
+								<li><a>Members</a></li>
+								<li><a>Past Screenings</a></li>
+								<li><a>Upcoming Screenings</a></li>
+							</ul>
+						</div>
+						<div class="col-md-4">
+							<ul>
+								<li id="search">
+									<form>
+										<span class="glyphicon glyphicon-search"></span>
+										<input type="search" name="search" placeholder="search" id="searchbox">
+									</form>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<div class="row logo">
+				<div class="col-md-12">
+					<h1 class="text-center title">PHILADELPHIA PSYCHOTRONIC SOCIETY</h1>
+				</div>
+			</div>
+		</header>
+		<section class="slider">
+		</section>
+		<section>
+			<div class="row funnyPic">
+				<div class="col-md-12">
+					<img src="">
+				</div>
+			</div>
+			<div class="container-fluid callouts">
+				<div class="row">
+	  				<div class="col-md-4 members">
+	  					<img src="./images/dragon_strikes.jpg" alt="The Dragon Strkes Again">
+						<h1>Members</h1>
+						<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum.</p>
+						<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
+					</div>
+	  				<div class="col-md-4 screenings">
+	  					<img src="./images/werewolf.jpeg">
+						<h1>Screenings</h1>
+						<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum.</p>
+						<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
+					</div>
+	  				<div class="col-md-4 upcoming">
+	  					<img src="./images/tourist.jpg">
+						<h1>Upcoming</h1>
+						<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum. Lorem ipsum dolor </p>
+						<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="about">
+			<h1 class="text-left">About this Thing!</h1>
+			<img src="./images/philamoca.jpg" class="philamoca">
+			<p class="about_text">We'll be carrying on the proud tradition set forth by Psychotronic Film Societies around the globe by screening cult/weirdo/trash films for the locally like-minded. Every second and last Monday of the month we'll meet at 7:30 PM at the Philadelphia Mausoleum of Contemporary Art to watch a feature film. All are invited to attend and contribute suggestions for future screenings, it'll be a programming round robin. And the Psychotronic Film Society tradition of watching films while drinking will be preserved so BYOB is encouraged.
+			<br>
+			<br>
+			The PPFS is officially sanctioned by Brian Thomas, Propaganda Minister of the Sinister for Chicago's Psychotronic Film Society.
+			<br>
+			<br>
+			WARNING: The subject matter of what we've come to know as psychotronic films runs the gamut from talking dogs to violent assaults. We will do our best to warn audiences in advance about films that contain extremely graphic and possibly upsetting content. That said, the very nature of many psychotronic films is to push the boundaries of taste, morals, and patience so always attend at your own risk.
+			<br>
+			<br>
+			Read a history of the Psychotronic Film Society here:
+			http://psychotronic.info/about/
+			</p>
+		</section>
+		<footer class="footer">
+			<div class="container-fluid">
+				<div class="row footer_row">
+					<div class="col-md-6">
+						<div class="address">
+							<p>@2017 WordPress Theme</p>
+							<p>123 Anytime St.</p>
+							<p>Haddon Township, NJ 08108
+						</div>
+					</div>
+					<div class="col-md-6">
+						<nav id="bottom_menu">
+							<ul>
+								<li><a>Home</a></li>
+								<li><a>About US</a></li>
+								<li><a>Blog</a></li>
+								<li><a>Contact Us</a></li>
+								<li><a>Members</a></li>
+								<li><a>Past Screenings</a></li>
+								<li><a>Upcoming Screenings</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</footer>		
+	</body>
+</html>
