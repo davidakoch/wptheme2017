801b5d9e8ae6e7ff62a8df1369a55b6c372eec2a
David A Koch <kodave1@gmail.com>
2017-10-20 23:39:38 -0400

    added page about

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 
-
 <?php get_header(); ?>
+
+
 
 <div class="row logo">
 	<div class="col-md-12">
 		<h1 class="text-center title">PHILADELPHIA PSYCHOTRONIC SOCIETY</h1>
 	</div>
 </div>		
-<section class="slider">
-</section>
-<div class="blog-post">
-	<?php if(have_posts()) : ?>
-		<?php while(have_posts()) : the_post(); ?>
-		<?php endwhile; ?>
-	<?php else : ?>
-		<p><?php __('No Posts Found'); ?></p>
-	<?php endif; ?>
-</div>
+
+
+<div id="primary" class="content-area">
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php the_title(); ?>
+<?php the_content(); ?>
+
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+
+
+
+
+</div><!-- #primary -->
 <section>
 	<div class="row funnyPic">	
 		<div class="col-md-12">
@@ -63,5 +72,5 @@
 	</p>
 </section>
 
-<?php get_footer(); ?>
 
+<?php get_footer(); ?>
