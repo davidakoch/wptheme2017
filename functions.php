89f2bba37dfbfed7d5b3d4bd1432dbc2163235bf
David A Koch <kodave1@gmail.com>
2017-10-27 21:41:04 -0400

    add theme support post thumbnails

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 
-register_nav_menus(array(
-	'primary' => __( 'Primary Menu'),
-	'footer' => __( 'Footer Menu'),
-));
+	add_theme_support('post-thumbnails');
 
-//Excerpt Length Control
+	register_nav_menus(array(
+		'primary' => __( 'Primary Menu'),
+		'footer' => __( 'Footer Menu'),
+	));
 
-function set_excerpt_length(){
-	return 50;
-}
+	//Excerpt Length Control
 
-add_filter('excerpt_length', 'set_excerpt_length');
+	function set_excerpt_length(){
+		return 50;
+	}
+
+	add_filter('excerpt_length', 'set_excerpt_length');
 
 ?>
 
