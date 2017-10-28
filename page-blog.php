3a365b17806167fa9e500797c7a54b4e9ca5a16b
David A Koch <kodave1@gmail.com>
2017-10-27 20:59:51 -0400

    added a tag to permalink

--- page-blog.php
+++ page-blog.php
@@ -21,8 +21,9 @@
 
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-	<h2><?php the_title(); ?></h2>
+	<a href="<?php the_permalink(); ?>">
+		<h2><?php the_title(); ?></h2>
+	</a>
 	<p><?php the_excerpt(); ?></p>
 	<strong><?php the_time('F jS, Y'); ?></strong> 
 	<div id="metadatas">
