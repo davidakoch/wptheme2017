e1ee0ee4f4cf97cd4a4923992b9dac1c43a73d91
David A Koch <kodave1@gmail.com>
2017-10-27 21:20:18 -0400

    finished single php file

--- single.php
+++ single.php
@@ -9,57 +9,22 @@
 	</div>
 </div>		
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="content-area">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-		<h2><?php the_title(); ?></h2>
-	
+		
+		<strong><?php the_time('F jS, Y'); ?></strong> 
+			<div id="metadatas">
+				<?php the_category(); ?>
+				<span id="tags"><?php the_tags(); ?></span>
+			</div>
 
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
 
-<section>
-	<div class="row funnyPic">	
-		<div class="col-md-12">
-			<img src="">
-		</div>
-	</div>
-	<div class="container-fluid callouts">
-		<div class="row">
-				<div class="col-md-4 members">
-					<img src="http://first-theme-sept-2017.dev/wp-content/uploads/2017/10/dragon_strikes.jpg" alt="The Dragon Strkes Again">
-				<h1>Members</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum.</p>
-				<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
-			</div>
-				<div class="col-md-4 screenings">
-					<img src="http://first-theme-sept-2017.dev/wp-content/uploads/2017/10/werewolf.jpeg">
-				<h1>Screenings</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum.</p>
-				<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
-			</div>
-				<div class="col-md-4 upcoming">
-					<img src="http://first-theme-sept-2017.dev/wp-content/uploads/2017/10/tourist.jpg">
-				<h1>Upcoming</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit ante nam sociis aptent felis ligula, neque pharetra massa gravida cum. Lorem ipsum dolor </p>
-				<p><a href="http://psychotronicphilly.org/">Learn More</a></p>
-			</div>
-		</div>
-	</div>
-</section>
-<section class="about">
-	<h1 class="text-left">About this Thing!</h1>
-	<img src="http://first-theme-sept-2017.dev/wp-content/uploads/2017/10/philamoca.jpg" class="philamoca">
-	<p class="about_text">We'll be carrying on the proud tradition set forth by Psychotronic Film Societies around the globe by screening cult/weirdo/trash films for the locally like-minded. Every second and last Monday of the month we'll meet at 7:30 PM at the Philadelphia Mausoleum of Contemporary Art to watch a feature film. All are invited to attend and contribute suggestions for future screenings, it'll be a programming round robin. And the Psychotronic Film Society tradition of watching films while drinking will be preserved so BYOB is encouraged.
-	<br>
-	<br>
-	The PPFS is officially sanctioned by Brian Thomas, Propaganda Minister of the Sinister for Chicago's Psychotronic Film Society.
-	<br>
-	<br>
-	WARNING: The subject matter of what we've come to know as psychotronic films runs the gamut from talking dogs to violent assaults. We will do our best to warn audiences in advance about films that contain extremely graphic and possibly upsetting content. That said, the very nature of many psychotronic films is to push the boundaries of taste, morals, and patience so always attend at your own risk.
-	<br>
-	<br>
-	Read a history of the Psychotronic Film Society here:
-	http://psychotronic.info/about/
-	</p>
-</section>
+</div>
 
 
 <?php get_footer(); ?>
