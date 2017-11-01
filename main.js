3a03391c4e7a387002786d948182a49bc9563d74
David A Koch <kodave1@gmail.com>
2017-10-31 21:35:33 -0400

    added some comment to main js

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 jQuery(document).ready(function($) {
 
 	$(document).on('ready', function(){
-//makes the 700px menu appear when you hit menu
+//makes the mobile list appear when you hit the menu button
 		$('#mobile_header').on('click', function(){
-			
-			console.log("hello");
 			$('#mobile_menu').css("height", 500);
 			$('#mobile_list').css("display","block");
 			$('#mobile_header').css("display","none");
-
-
 		});
 
+//brings back the menu button when you hit the menu list
 		$('#mobile_list').on('click', function(){
 			$('#mobile_header').css("display","block");
 			$('#mobile_list').css("display", "none");
