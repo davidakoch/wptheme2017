2dad720061825f87d7274595a89e1d2399a5e1a5
David A Koch <kodave1@gmail.com>
2017-11-08 23:37:23 -0500

    content area page css for page php

--- page.php
+++ page.php
@@ -5,9 +5,19 @@
 	</div>
 </div>		
 
+<div id="primary" class="content-area-page">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<h1><?php the_title(); ?></h1>
+		<?php if(has_post_thumbnail()) : ?>
+			<?php the_post_thumbnail(); ?>
+		<?php endif; ?>
+		<?php the_content(); ?>
 
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no pages matched your criteria.' ); ?></p>
+	<?php endif; ?>
 
-
+</div>
 
 
 <section class="about">
