5a24b91b47f5db38825d0016163233199f11a1a6
David A Koch <kodave1@gmail.com>
2017-11-12 17:28:33 -0500

    finished woocommerce page

--- woocommerce.php
+++ woocommerce.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php 
+
+/*
+	Template Name: Woocommerce
+*/
+
+get_header(); ?>
 <div class="row logo">
 	<div class="col-md-12">
 		<h1 class="text-center title"><a href="<?php echo home_url(); ?>">PHILADELPHIA PSYCHOTRONIC SOCIETY</a></h1>
@@ -6,16 +12,8 @@
 </div>		
 
 <div id="primary" class="content-area-page">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<?php if(has_post_thumbnail()) : ?>
-			<?php the_post_thumbnail(); ?>
-		<?php endif; ?>
-		<?php the_content(); ?>
 
-	<?php endwhile; else : ?>
-		<p><?php esc_html_e( 'Sorry, no pages matched your criteria.' ); ?></p>
-	<?php endif; ?>
+	<?php woocommerce_content(); ?>
 
 </div>
 
