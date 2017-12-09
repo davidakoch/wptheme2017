1c8da6dc1ff7e0e4d1dfcc83b991fa8c17f30524
David A Koch <kodave1@gmail.com>
2017-12-09 15:21:34 -0500

    finsihed code to sidebar php

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,5 @@
-<div id="sidebar-primary" class="sidebar">
-    <?php dynamic_sidebar( 'sidebar' ); ?>
-</div>
+<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+	<div id="sidebar-primary" class="sidebar">
+	    <?php dynamic_sidebar( 'sidebar' ); ?>
+	</div>
+<?php endif; ?>
