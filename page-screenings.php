1a67cefe90416c56d46db5f572758e899e616f20
David A Koch <kodave1@gmail.com>
2017-12-09 15:48:08 -0500

    added sidebar to upcoming screenings

--- page-screenings.php
+++ page-screenings.php
@@ -11,21 +11,21 @@ get_header(); ?>
 	</div>
 </div>		
 
-<div id="primary" class="content-area-page">
+<div class="wrap">
+	<div id="primary-screenings" class="col-md-8">	
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+			<?php if(has_post_thumbnail()) : ?>
+				<?php the_post_thumbnail(); ?>
+			<?php endif; ?>
+			<?php the_content(); ?>
+
+		<?php endwhile; else : ?>
+			<p><?php esc_html_e( 'Sorry, no pages matched your criteria.' ); ?></p>
+		<?php endif; ?>
+	</div>
 	<!--here is where I added the sidebar-->
 	<?php get_sidebar( 'Sidebar' ); ?>
-	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<?php if(has_post_thumbnail()) : ?>
-			<?php the_post_thumbnail(); ?>
-		<?php endif; ?>
-		<?php the_content(); ?>
-
-	<?php endwhile; else : ?>
-		<p><?php esc_html_e( 'Sorry, no pages matched your criteria.' ); ?></p>
-	<?php endif; ?>
-
 </div>
 
 
