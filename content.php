a41a4e5432f1b79c847444a10fd071f3bff85309
David A Koch <kodave1@gmail.com>
2017-12-15 22:43:53 -0500

    change the title in single php to just a title

--- content.php
+++ content.php
@@ -1,7 +1,12 @@
-	<!--this turns the post title into a permalink-->
+	<!--this turns the post title into just a title if it is single post-->
+	<?php if(is_single()) : ?>
+		<h2><?php the_title(); ?></h2>
+	<!--this turns the post excerpt on the blog page into a title with a permalink-->
+	<?php else : ?>
 	<a href="<?php the_permalink(); ?>">
 		<h2><?php the_title(); ?></h2>
 	</a>
+		<?php endif; ?>
 		<?php if(is_single()) : ?>
 			<!--get the content if it is single-->
 			<?php the_content(); ?>
