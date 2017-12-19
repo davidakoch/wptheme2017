e517852917aa61b252c9faa09f497653379f5dc4
David A Koch <kodave1@gmail.com>
2017-12-18 23:35:41 -0500

    added customize register hook to customizer php

---
+++ inc/customizer.php
@@ -0,0 +1,10 @@
+<?php 
+	//an attempt to add new functions to the customizer
+	function bronson_customize_register(){
+
+	}
+
+	add_action('customize_register', 'bronson_customize_register');
+
+
+ ?>
